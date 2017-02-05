3351cb71f30388855fe7b36bceb1bc8b3c63c932
aktunc-STMU <aktunc@gmail.com>
2017-02-05 00:53:16 -0600

    Incorrect result, check comments

--- Drink.java
+++ Drink.java
@@ -10,6 +10,9 @@ public class Drink {
 		double costumers = 12467;
 		double amount_purchased= costumers * .14;
 		double citrus= costumers * .64;
+		/*this is not correct as 64 percent of energy drink users prefer citrus not 
+		64 percent of the customers, should be .14 * .64 * customers. 
+		*/
 		
 		System.out.println("The number of costumers that purchase one or more energy drinks per week is " + df.format(amount_purchased));
 		System.out.println("The number of costumers that in the survey who prefer citrus flavored energy drinks is " + df.format(citrus));
