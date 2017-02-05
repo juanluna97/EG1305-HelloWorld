b0c4a4f8432c99f95acfd9cc9727b659de7774eb
juanluna97 <jlunaglaser@mail.stmarytx.edu>
2017-02-05 01:41:49 -0600

    Update Drink.java

--- Drink.java
+++ Drink.java
@@ -9,10 +9,7 @@ public class Drink {
 		DecimalFormat df = new DecimalFormat ("0");
 		double costumers = 12467;
 		double amount_purchased= costumers * .14;
-		double citrus= costumers * .64;
-		/*this is not correct as 64 percent of energy drink users prefer citrus not 
-		64 percent of the customers, should be .14 * .64 * customers. 
-		*/
+		double citrus= amount_purchased * .64 ;
 		
 		System.out.println("The number of costumers that purchase one or more energy drinks per week is " + df.format(amount_purchased));
 		System.out.println("The number of costumers that in the survey who prefer citrus flavored energy drinks is " + df.format(citrus));
