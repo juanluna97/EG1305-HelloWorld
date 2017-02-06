8a4050d28b99cab20396d27152d1ac26b00ccf72
juanluna97 <jlunaglaser@mail.stmarytx.edu>
2017-02-06 10:42:35 -0600

    Update ComputeBMI.java

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -5,17 +5,23 @@
 /*The purpose of this code is to use JOptionPane to prompt the user for their height
  * and weight in inches and kg. This will convert that into their respective units and 
  * calculate the BMI.*/
+
 import javax.swing.JOptionPane;
 public class ComputeBMI {
+	
 	public static void main (String [] args){
-		//Asking the user for the information
-		String UserWeight = JOptionPane.showInputDialog("Enter your weight in pounds. ");
-		double Weight = Double.parseDouble(UserWeight);
-		String UserHeight = JOptionPane.showInputDialog("Enter your height in inches. ");
-		double Height = Double.parseDouble(UserHeight);
-		//Calculations are done here
-		double BMI = (Weight * 0.45359237) / ((Height * 0.0254)*(Height * 0.0254));
-		//Printing results
-		JOptionPane.showMessageDialog( null, "Your Body Mass Index is " + BMI);
+		
+	//Asking the user for the information
+	String UserWeight = JOptionPane.showInputDialog("Enter your weight in pounds. ");
+	double Weight = Double.parseDouble(UserWeight);
+		
+	String UserHeight = JOptionPane.showInputDialog("Enter your height in inches. ");
+	double Height = Double.parseDouble(UserHeight);
+		
+	//Calculations are done here
+	double BMI = (Weight * 0.45359237) / ((Height * 0.0254)*(Height * 0.0254));
+		
+	//Printing results
+	JOptionPane.showMessageDialog( null, "Your Body Mass Index is " + BMI);
 	}
 }
