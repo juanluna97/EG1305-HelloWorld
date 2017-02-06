c33afe8fbdbecb577d49e68cae54ac005722f936
juanluna97 <jlunaglaser@mail.stmarytx.edu>
2017-02-06 10:12:50 -0600

    Update CountMoney.java

--- CountMoney.java
+++ CountMoney.java
@@ -4,16 +4,24 @@
 //5 February 2017
 /*The purpose of this code is to ask the user for a quantity of money and divide that
  *money into their subsequent divisions of coins and dollars by using a scanner function*/
+
 import java.util.Scanner;
+
 public class CountMoney {
+	
 	public static void main (String [] args){
+		
 		Scanner input = new Scanner(System.in);
+		
 		try{
+		
 		//Asking the user for the money in integers
 		System.out.println("Enter an amount in integer, for example 1156");
 		System.out.print("for 11 dollars and 56 cents: ");
 		int num = input.nextInt();
+			
 		System.out.println("Your amount of " + num + " consists of");
+		
 		//Calculations of the conversion
 		int cents = num % 100; 
 		int dollars = (num - cents) / 100;
@@ -21,12 +29,14 @@ public class CountMoney {
 		int dimes = (cents - (25*quarters)) / 10;
 		int nickels = (cents - ((25*quarters)+(10*dimes))) / 5;
 		int pennies = (cents - ((25*quarters)+(10*dimes)+(5*nickels)));
+		
 		//Printing results
 		System.out.println(dollars + " dollars");
 		System.out.println(quarters + " quarters");
 		System.out.println(dimes + " dimes");
 		System.out.println(nickels + " nickels");
 		System.out.println(pennies + " pennies");}
+		
 		//Closing the scanner function
 		finally{
 			input.close();}
